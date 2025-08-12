fc3d2729e6ce2301d70b8fecb0321786ef4100b9
simchaschaumnice <simcha.schaum@nice.com>
2025-08-12 22:40:50 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="landing-title-container">
+            {/* Replace the src below with your logo image path */}
+            <img
+              src="./src/images/New Lomdei Logo.png"
+              alt="Lomdei Logo"
+              className="lomdei-logo"
+            />
+            <h1>
+              Lomdei bridges tradition and innovation, empowering educators to create personalized Jewish learning experiences accessible to all students.
+            </h1>
+        </div>
+    </div>
+   
+  );
+}
+
+export default App;
