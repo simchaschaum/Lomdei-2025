365cce44c0e9b40046b0e16b489e0c252d5c7b90
simchaschaumnice <simcha.schaum@nice.com>
2025-10-19 23:27:34 +0300

    Update Vite configuration to set output directory to 'docs'

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Lomdei-2025/', // must exactly match repo name
   plugins: [react()],
-  base: '/Lomdei-2025/', // This is correct for GitHub Pages project sites
   build: {
     outDir: 'docs' // This will output your build to the docs folder
   }
-})
+});
